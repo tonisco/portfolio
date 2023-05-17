23e517e52a4cf75dcf55e329149d814c20570b4a
tonisco <tonyumeozulu@gmail.com>
2023-05-17 08:05:16 +0100

    gr

--- src/components/Projects/projectList.ts
+++ src/components/Projects/projectList.ts
@@ -12,7 +12,7 @@ export const projects = [
     The restaurant web app is where restaurants create their profile, list their food products and manage their orders with ease as wells view their sales and most sold food item.
     The driver app is an app that the driver uses to find available orders ready for pick up and the delivery location. He uses the directions on the app as a guide to deliver the food items
     The food app is the app the customers use to view the menus of all the restaurants around them, order food items from their favorite restaurants and track and manage their orders.
-    The api backend manages all the action and interactions of all this apps and also store all this data thr database`,
+    The api backend manages all the action and interactions of all this apps and also store all this data in the database`,
     short_description:
       "A complete project written a turbo-repo environment for start-up organization that specializes in providing local restaurants a platform to sell their foods, job creation through the platform for drivers to deliver those foods and a platform where users can browse available restaurants around them and purchase food have have it delivered to them.",
     majorLibraries: [
